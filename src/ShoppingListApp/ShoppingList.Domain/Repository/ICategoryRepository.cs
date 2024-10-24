ed287c20c4a3fe059ba066097ad849a69a9da746
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-10-24 20:10:25 +0100

    Add interfaces

---
+++ src/ShoppingListApp/ShoppingList.Domain/Repository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingList.Domain.Models;
+using ShoppingList.Domain.SeedWork;
+
+namespace ShoppingList.Domain.Repository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        Task<Category> FindByNameAsync(string name);
+        Task<List<Category>> FindAllByNameStartedWithAsync(string text);
+    }
+}
---
+++ src/ShoppingListApp/ShoppingList.Domain/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingList.Domain.Models;
+using ShoppingList.Domain.SeedWork;
+
+namespace ShoppingList.Domain.Repository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<Product> FindByNameAsync(string name);
+    }
+}
