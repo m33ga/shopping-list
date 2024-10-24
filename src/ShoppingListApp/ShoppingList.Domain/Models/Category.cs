3394ad1c4a8c2f81782f60c1db3c06e2bffe5a9e
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-10-24 20:09:23 +0100

    Add class models

---
+++ src/ShoppingListApp/ShoppingList.Domain/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShoppingList.Domain.SeedWork;
+
+namespace ShoppingList.Domain.Models
+{
+    public class Category : Entity
+    {
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ src/ShoppingListApp/ShoppingList.Domain/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShoppingList.Domain.SeedWork;
+
+namespace ShoppingList.Domain.Models
+{
+    public class Product : Entity
+    {
+        public string Name { get; set; }
+        public byte[] Thumb { get; set; } // thumbnail (image of product)
+        public int CategoryId { get; set; } // https://www.entityframeworktutorial.net/efcore/entity-framework-core.aspx example 4
+        public Category Category { get; set; } 
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
