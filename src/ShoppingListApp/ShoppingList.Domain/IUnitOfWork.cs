9a6e16323f6deee0a87721b722cc39393d9189d9
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-10-24 20:11:26 +0100

    Add unit of work

---
+++ src/ShoppingListApp/ShoppingList.Domain/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingList.Domain.Repository;
+
+namespace ShoppingList.Domain
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        // deal with database operations -> IDisposable
+        // bridge between repos and rest of code
+        IProductRepository ProductRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+
+        Task SaveAsync();
+    }
+}
