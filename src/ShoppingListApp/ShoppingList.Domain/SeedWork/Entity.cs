90ed122751565d6fb4b45620cfd08c32ce1bbb23
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-10-24 20:09:06 +0100

    Add SeedWork

---
+++ src/ShoppingListApp/ShoppingList.Domain/SeedWork/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingList.Domain.SeedWork
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+
+
+    }
+}
---
+++ src/ShoppingListApp/ShoppingList.Domain/SeedWork/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Domain.SeedWork
+{
+    public interface IRepository<T> where T : Entity
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<T> FindOrCreateAsync(T entity);
+        Task<T> FindByIdAsync(int id);
+        Task<List<T>> FindAllAsync();
+
+    }
+}
