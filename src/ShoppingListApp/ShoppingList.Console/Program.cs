ab32c084eadfcfa9d7c74129d1536b33cf60c03d
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-10-24 20:10:41 +0100

    Add conosle app

---
+++ src/ShoppingListApp/ShoppingList.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//using System.ComponentModel;
+using ShoppingList.Domain.Models;
+
+Console.WriteLine("Shopping list testing in console");
+
+var c1 = new Category
+{
+    Name = "Drinks"
+};
+
+var p1 = new Product();
+p1.Name = "Beer";
+var p2 = new Product();
+p2.Name = "Wine";
+
+
+c1.Products.Add(p1);
+c1.Products.Add(p2);
+Console.WriteLine($"Category {c1}");
+for (int i = 0; i < c1.Products.Count; i++)
+{
+    Console.WriteLine($"- Prod {i+1} : {c1.Products[i].Name}");
+}
