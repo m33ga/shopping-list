af330795baf3aa2c8858f780cdf242e7baca94de
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-10-29 19:24:47 +0000

    Add shopping list db context

---
+++ src/ShoppingListApp/ShoppingList.Infrastructure/ShoppingListDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using ShoppingList.Domain.Models;
+
+namespace ShoppingList.Infrastructure
+{
+    public class ShoppingListDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public string DbPath { get; }
+
+        public ShoppingListDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Combine(path, "2024ShoppingListClassA.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+
+        // fluent api
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasIndex(x => x.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Product>()
+                .Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Category>()
+                .HasIndex(x => x.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Category>()
+                .Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+
+    }
+}
